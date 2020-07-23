cabeb548cd162e2d2afcb0cb9c4425d2275effad
cmsunu28 <christine.sunu@gmail.com>
2020-07-23 14:04:31 -0700

    new organization and prep for ep 2)

--- couplet.js
+++ couplet.js
@@ -1,4 +1,7 @@
 // To make sure my modules are awesome and fire
 // I will start this node script by saying 'require'
 
-require 
+require('dotenv').config();
+
+// make sure you call this as soon as you can
+// if environment variables are a part of your plan
---
+++ pictionary/guesstimer.js
@@ -0,0 +1,23 @@
+// la la la copying and pasting and editing from stack overflow is my jam
+
+var i = 5*60;
+
+function startTimer() {
+
+    var countdownTimer = setInterval(function() {
+
+        var min=((i-(i%60))/60);
+        var sec=i%60;
+        if (sec<10) {sec="0"+sec;}
+        console.log(min+':'+sec);
+        i = i - 1;
+
+        if (i <= 0) {
+            clearInterval(countdownTimer);
+        }
+
+    }, 1000);
+
+}
+
+startTimer(); 
